c257ee054468ef767a7cd4aefbf172c19e4e8a75
Shreyash <shreyash@chonkie.ai>
2025-03-30 22:38:54 -0700

    Add some test fixtures but we'll add tests later

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,3 +1,12 @@
+import tiktoken
+from chonkie.chunkers.tokenizer import (
+    CharacterTokenizer,
+    WordTokenizer,
+    Tokenizer,
+)
+from tokenizers import Tokenizer as HFTokenizer
+from transformers import AutoTokenizer
+from typing import Callable
 import pytest
 
 from typing import List
@@ -25,6 +34,7 @@ def sample_text() -> str:
     Testing should cover various scenarios, including text with short sentences, 
     long sentences, multiple paragraphs, and potentially unusual punctuation or spacing."""
 
+
 @pytest.fixture
 def sample_text_list() -> List[str]:
     """Fixture to provide a list of sample text for testing."""
@@ -38,6 +48,41 @@ def sample_text_list() -> List[str]:
         "Other applications include displaying text incrementally in user interfaces or preparing data for certain types of linguistic analysis.",
         "Effective chunking might consider sentence boundaries (using periods, question marks, exclamation points), paragraph breaks (often marked by double newlines), or simply aim for fixed-size chunks based on character or word counts.",
         "The ideal strategy depends heavily on the specific downstream application.",
-        "Testing should cover various scenarios, including text with short sentences, long sentences, multiple paragraphs, and potentially unusual punctuation or spacing."
+        "Testing should cover various scenarios, including text with short sentences, long sentences, multiple paragraphs, and potentially unusual punctuation or spacing.",
     ]
 
+
+@pytest.fixture
+def char_tokenizer() -> CharacterTokenizer:
+    """Character tokenizer fixture."""
+    return CharacterTokenizer()
+
+
+@pytest.fixture
+def word_tokenizer() -> WordTokenizer:
+    """Word tokenizer fixture."""
+    return WordTokenizer()
+
+
+@pytest.fixture
+def hf_tokenizer() -> HFTokenizer:
+    """Create a HuggingFace tokenizer fixture."""
+    return HFTokenizer.from_pretrained("gpt2")
+
+
+@pytest.fixture
+def tiktoken_tokenizer() -> tiktoken.Encoding:
+    """Create a Tiktoken tokenizer fixture."""
+    return tiktoken.get_encoding("gpt2")
+
+
+@pytest.fixture
+def transformers_tokenizer() -> AutoTokenizer:
+    """Create a Transformer tokenizer fixture."""
+    return AutoTokenizer.from_pretrained("gpt2")
+
+
+@pytest.fixture
+def callable_tokenizer() -> Callable[[str], int]:
+    """Create a callable tokenizer fixture."""
+    return lambda text: len(text.split())
