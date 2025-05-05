f0f4aee5c7ab34c9958b0c1b68353e75ea73e3fc
Bhavnick @ chonkie.ai <bhavnick@chonkie.ai>
2025-05-05 20:00:04 +0530

    Feat: Add the `Chomp` base class

---
+++ src/chonkie/chomp/pipeline.py
@@ -0,0 +1,9 @@
+"""Module for Chonkie's multi-step pipeline."""
+
+class Chomp: 
+    """Chonkie's multi-step pipeline."""
+
+    def __init__(self):
+        """Initialize the pipeline."""
+        pass
+    
