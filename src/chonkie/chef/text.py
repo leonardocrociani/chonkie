f009bdfb273b981405255257f5387f112d609ad4
Bhavnick @ chonkie.ai <bhavnick@chonkie.ai>
2025-07-21 17:47:13 -0700

    feat: Implement TextChef and FileFetcher
    This commit introduces two new components: TextChef and FileFetcher.
    TextChef handles text data processing, while FileFetcher retrieves files
    from a specified directory.

--- src/chonkie/chef/text.py
+++ src/chonkie/chef/text.py
@@ -1,15 +1,33 @@
 """TextChef is a chef that processes text data."""
 
+from pathlib import Path
+from typing import List, Union
+
 from .base import BaseChef
 
 
 class TextChef(BaseChef):
     """TextChef is a chef that processes text data."""
 
-    def process(self, path: str) -> str:
+    def process(self, path: Union[str, Path]) -> str:
         """Process the text data."""
         with open(path, "r") as file:
-            return file.read()
+            return str(file.read())
+
+    def process_batch(self, paths: Union[List[str], List[Path]]) -> List[str]:
+        """Process the text data in a batch."""
+        return [self.process(path) for path in paths]
+
+    def __call__(
+        self, path: Union[str, Path, List[str], List[Path]]
+    ) -> Union[str, List[str]]:
+        """Fetch data from the file."""
+        if isinstance(path, (list, tuple)):
+            return self.process_batch(path)
+        elif isinstance(path, (str, Path)):
+            return self.process(path)
+        else:
+            raise TypeError(f"Unsupported type: {type(path)}")
 
     def __repr__(self) -> str:
         """Return a string representation of the chef."""
--- src/chonkie/fetcher/file.py
+++ src/chonkie/fetcher/file.py
@@ -1,5 +1,8 @@
 """FileFetcher is a fetcher that fetches data from a file."""
 
+from pathlib import Path
+from typing import List, Optional
+
 from .base import BaseFetcher
 
 
@@ -10,21 +13,37 @@ class FileFetcher(BaseFetcher):
         """Initialize the FileFetcher."""
         super().__init__()
 
-    def fetch(self, path: str) -> str:
+    def fetch(self, dir: str, ext: Optional[List[str]] = None) -> List[Path]:
         """Fetch data from the file.
 
         Returns:
             str: The data fetched from the file.
 
         """
-        with open(path, "r") as f:
-            return str(f.read())
+        # Reads the entire directory and returns a list of files with the specified extension
+        return [
+            file
+            for file in Path(dir).iterdir()
+            if file.is_file() and (ext is None or file.suffix in ext)
+        ]
 
-    def __call__(self, path: str) -> str:
+    def fetch_file(self, dir: str, name: str) -> Path:
+        """Given a directory and a file name, return the path to the file.
+
+        NOTE: This method is mostly for uniformity across fetchers since one may require to
+        get a file from an online database.
+        """
+        # We should search the directory for the file
+        for file in Path(dir).iterdir():
+            if file.is_file() and file.name == name:
+                return file
+        raise FileNotFoundError(f"File {name} not found in directory {dir}")
+
+    def __call__(self, dir: str, ext: Optional[List[str]] = None) -> List[Path]:
         """Fetch data from the file.
 
         Returns:
             str: The data fetched from the file.
 
         """
-        return self.fetch(path)
+        return self.fetch(dir, ext)
