7ed25e223f56018e443614f6c3519a75b5b0ffc7
Bhavnick @ chonkie.ai <bhavnick@chonkie.ai>
2025-07-21 18:23:00 -0700

    Refactor TextChef docstrings

--- src/chonkie/chef/text.py
+++ src/chonkie/chef/text.py
@@ -10,18 +10,34 @@ class TextChef(BaseChef):
     """TextChef is a chef that processes text data."""
 
     def process(self, path: Union[str, Path]) -> str:
-        """Process the text data."""
+        """Process the text data from given file(s).
+
+        Args:
+            path (Union[str, Path]): Path to the file(s) to process.
+
+        Returns:
+            str: Processed text data.
+
+        """
         with open(path, "r") as file:
             return str(file.read())
 
     def process_batch(self, paths: Union[List[str], List[Path]]) -> List[str]:
-        """Process the text data in a batch."""
+        """Process the text data in a batch.
+
+        Args:
+            paths (Union[List[str], List[Path]]): Paths to the files to process.
+
+        Returns:
+            List[str]: Processed text data.
+
+        """
         return [self.process(path) for path in paths]
 
     def __call__(
         self, path: Union[str, Path, List[str], List[Path]]
     ) -> Union[str, List[str]]:
-        """Fetch data from the file."""
+        """Process the text data from given file(s)."""
         if isinstance(path, (list, tuple)):
             return self.process_batch(path)
         elif isinstance(path, (str, Path)):
